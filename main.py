6d235c0ba65d0fda7a4e2610c112fc96c86fb7a2
Antonio Sandoval <antonio.sandoval@posteo.net>
2025-07-02 12:57:25 -0600

    FPS capped to 144

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    fps_limit = 60
+    fps_limit = 144
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     player = Player(x, y)
