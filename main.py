67f2b5277570eb0862de37b0fea929ef5e037d05
ajsandovalt <antonio.sandoval@posteo.net>
2026-04-23 17:58:07 -0600

    Working on player movement

--- main.py
+++ main.py
@@ -10,13 +10,11 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    # Now we set the screen properties 
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    # Instatiate player
-
     player = Player( SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0 # dt = Delta Time
 
     # Initiate game loop
 
@@ -29,7 +27,9 @@ def main():
 
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
